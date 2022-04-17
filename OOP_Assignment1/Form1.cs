35ca756a2297ff41fc5aab58720927e0278b7575
Alver <Alver@DESKTOP-NK6FC4K>
2022-04-17 03:03:07 -0400

    First Commit

---
+++ OOP_Assignment1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Assignment1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you want to know my name?", "Hi", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk) == DialogResult.Yes)
+            {
+                MessageBox.Show("Hi. I am Jose Alver H. Estacion");
+            }
+            else
+            {
+                MessageBox.Show("Okay, Bye");
+            }
+        }
+    }
+}
